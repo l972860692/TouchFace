2d32af60eddf5e04bb701baf4c6a309cbe4c4b01
liusong <972860692@qq.com>
2020-07-26 23:11:21 +0800

    添加项目文件。

---
+++ FaceDetectionDemo/FormDemo.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace FaceDetectionDemo
+{
+    partial class FormDemo
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.FacePictureBox = new System.Windows.Forms.PictureBox();
+            this.TimerDetector = new System.Windows.Forms.Timer(this.components);
+            this.VideoPlayer = new AForge.Controls.VideoSourcePlayer();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.FacePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonStart.Location = new System.Drawing.Point(846, 14);
+            this.ButtonStart.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(143, 73);
+            this.ButtonStart.TabIndex = 1;
+            this.ButtonStart.Text = "打开摄像头并识别人脸";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(846, 95);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(142, 26);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // FacePictureBox
+            // 
+            this.FacePictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FacePictureBox.Location = new System.Drawing.Point(14, 14);
+            this.FacePictureBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.FacePictureBox.Name = "FacePictureBox";
+            this.FacePictureBox.Size = new System.Drawing.Size(722, 502);
+            this.FacePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.FacePictureBox.TabIndex = 4;
+            this.FacePictureBox.TabStop = false;
+            // 
+            // VideoPlayer
+            // 
+            this.VideoPlayer.Location = new System.Drawing.Point(29, 28);
+            this.VideoPlayer.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.VideoPlayer.Name = "VideoPlayer";
+            this.VideoPlayer.Size = new System.Drawing.Size(271, 202);
+            this.VideoPlayer.TabIndex = 5;
+            this.VideoPlayer.Text = "videoSourcePlayer1";
+            this.VideoPlayer.VideoSource = null;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(846, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // FormDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 530);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.VideoPlayer);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ButtonStart);
+            this.Controls.Add(this.FacePictureBox);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormDemo";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Closing);
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.FacePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ButtonStart;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox FacePictureBox;
+        private System.Windows.Forms.Timer TimerDetector;
+        private AForge.Controls.VideoSourcePlayer VideoPlayer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FaceDetectionDemo/FormDemo.cs
@@ -0,0 +1,191 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ViewFaceCore.Sharp;
+using ViewFaceCore.Sharp.Model;
+
+namespace FaceDetectionDemo
+{
+    public partial class FormDemo : Form
+    {
+        public FormDemo()
+        {
+            InitializeComponent();
+
+            TimerDetector.Interval = 20 / 15; // 15 FPS
+            TimerDetector.Tick += TimerDetector_Tick;
+
+            VideoPlayer.Visible = false; // 隐藏摄像头画面控件
+        }
+
+        /// <summary>
+        /// 摄像头设备信息集合
+        /// </summary>
+        FilterInfoCollection VideoDevices;
+        /// <summary>
+        /// 人脸位置信息集合
+        /// </summary>
+        List<Rectangle> FaceRectangles = new List<Rectangle>();
+
+        /// <summary>
+        /// 人脸识别库
+        /// </summary>
+        ViewFace ViewFace = new ViewFace();
+
+        /// <summary>
+        /// 窗体加载时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form_Load(object sender, EventArgs e)
+        {
+            VideoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            comboBox1.Items.Clear();
+            foreach (FilterInfo info in VideoDevices)
+            {
+                comboBox1.Items.Add(info.Name);
+            }
+            if (comboBox1.Items.Count > 0)
+            { comboBox1.SelectedIndex = 0; }
+        }
+
+        /// <summary>
+        /// 点击开始按钮时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+
+            #region 识别老照片
+            DirectoryInfo TheFolder = new DirectoryInfo(@"C:\Users\mrls\Desktop\video");
+            if (!TheFolder.Exists)
+            {
+
+                MessageBox.Show("文件夹不存在");
+                return;
+            }
+            var files = TheFolder.GetFiles().Where(x => ".jpg,.png,".Contains(x.Extension.ToLower() + "")).ToList();
+
+            foreach (FileInfo NextFile in files)
+            {
+
+                float[] oldEigenValues;
+                Bitmap oldImg = (Bitmap)Image.FromFile(NextFile.FullName);   
+                var oldFaces = ViewFace.FaceDetector(oldImg);   
+                if (oldFaces.Length > 0)  
+                {
+
+                    var oldPoints = ViewFace.FaceMark(oldImg, oldFaces[0]);  
+                    oldEigenValues = ViewFace.Extract(oldImg, oldPoints);  
+                }
+                else
+                {
+                    oldEigenValues = new float[0];  
+                }
+                var person = new PersonInfo();
+                person.Name = NextFile.Name.Substring(0, NextFile.Name.IndexOf("."));
+                person.oldEigenValues = oldEigenValues;
+                personInfos.Add(person);
+
+            }
+
+            #endregion
+            if (VideoPlayer.IsRunning)
+            {
+                VideoPlayer.SignalToStop();
+                VideoPlayer.WaitForStop();
+                TimerDetector.Start();
+                ButtonStart.Text = "打开摄像头并识别人脸";
+            }
+            else
+            {
+                if (comboBox1.SelectedIndex == -1) return;
+                FilterInfo info = VideoDevices[comboBox1.SelectedIndex];
+                VideoCaptureDevice videoCapture = new VideoCaptureDevice(info.MonikerString);
+                VideoPlayer.VideoSource = videoCapture;
+                VideoPlayer.Start();
+                TimerDetector.Start();
+                ButtonStart.Text = "关闭摄像头";
+            }
+        }
+        public static List<PersonInfo> personInfos = new List<PersonInfo>();
+    
+        private void TimerDetector_Tick(object sender, EventArgs e)
+        {
+            //label1.Text = "";
+            if (VideoPlayer.IsRunning)
+            {
+
+
+                Bitmap bitmap = VideoPlayer.GetCurrentVideoFrame(); // 获取摄像头画面
+
+                if (bitmap != null)
+                {
+                    var infos = ViewFace.FaceDetector(bitmap); // 识别画面中的人脸
+                    FaceRectangles.Clear();
+                    var test = ViewFace.FaceType;
+
+                    foreach (var info in infos)
+                    {
+                        FaceRectangles.Add(new Rectangle(info.Location.X, info.Location.Y, info.Location.Width, info.Location.Height));
+
+                        // 识别 bitmap 中指定的人脸信息 info 的关键点坐标。
+                        var faceps = ViewFace.FaceMark(bitmap, info);
+                        var s = ViewFace.Extract(bitmap, faceps);
+                        //bitmap.Save($"{DateTime.Now.Ticks}.jpg");
+
+                        personInfos.Where(x => true || x.Name == "刘松").ToList().ForEach(x =>
+                             {
+
+                                 float similarity = ViewFace.Similarity(x.oldEigenValues, s); // 对比两张照片上的数据，确认是否是同一个人。
+
+
+                            if (ViewFace.IsSelf(similarity))
+                                 {
+                                     label1.Text = $"{x.Name} ";
+
+                                //label1.Text = $"{similarity} ";
+
+                            }
+
+
+                             });
+
+
+                    }
+                    if (FaceRectangles.Count > 0) 
+                    {
+                        using (Graphics g = Graphics.FromImage(bitmap))
+                        {
+                            g.DrawRectangles(new Pen(Color.Red, 4), FaceRectangles.ToArray());
+                        }
+
+                    }
+                }
+                FacePictureBox.Image = bitmap;
+
+            }
+        }
+
+        /// <summary>
+        /// 窗体关闭时，关闭摄像头
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form_Closing(object sender, FormClosingEventArgs e)
+        {
+            VideoPlayer.SignalToStop();
+            VideoPlayer.WaitForStop();
+        }
+    }
+}
